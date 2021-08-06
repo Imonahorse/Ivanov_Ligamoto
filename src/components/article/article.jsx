4575b2a5239dd733da01a14c24c90c50e348f27d
Alexey <Awotjan@gmail.com>
2021-08-06 19:32:01 +0300

    Добавит слайдер

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import cn from 'classnames';
 import styles from './article.module.scss';
-import leftButton from './../../img/left-button.svg';
-import rightButton from './../../img/right-button.svg';
-import firstSlide from '../../img/slide-one.jpg';
-import secondSlide from '../../img/slide-two.jpg';
-import thirdSlide from '../../img/slide-three.jpg';
-
+import Slider from '../slider/slider';
 
 function Article() {
   const articleContainer = cn('container', styles.section);
@@ -16,21 +11,7 @@ function Article() {
       <h2 className='visually-hidden'>Основная информация</h2>
       <article className={styles.article}>
         <div className={styles.left}>
-          <div className={styles.promo}>
-            <span className={styles.label}>new model</span>
-            <img src={firstSlide} alt="Фото автомобиля"/>
-          </div>
-          <div className={styles.slider}>
-            <button className={styles.button} type='button'>
-              <img src={leftButton} alt="Кнопка слайдера влево"/>
-            </button>
-            <img className={styles.img} src={firstSlide} alt="Фото автомобиля"/>
-            <img className={styles.img} src={thirdSlide} alt="Фото автомобиля"/>
-            <img className={styles.img} src={secondSlide} alt="Фото автомобиля"/>
-            <button className={styles.button} type='button'>
-              <img src={rightButton} alt="Кнопка слайдера вправо"/>
-            </button>
-          </div>
+          <Slider />
         </div>
         <div>
           <h3 className={styles.title}>Марпех 11</h3>
